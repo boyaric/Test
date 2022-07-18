eb1da1ae056f9cd848aa84b7ebeacb07296d031f
Sergei_Kravtsov <boyaric@gmail.com>
2022-07-18 23:46:49 +0300

    checking array elements and displaying the result

--- Program.cs
+++ Program.cs
@@ -11,3 +11,35 @@ void FillNewArray (string[] inputArray)
         inputArray [i] = Console.ReadLine();
     }
 }
+string[] OutputArray (string[] inputArray)
+{
+    int count = inputArray.Length;
+    string[] outputArray = new string[count];
+    int index = 0;
+    for (int i=0; i<count; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            outputArray[index]= inputArray[i];
+            index +=1;
+        }
+    }
+    Array.Resize(ref outputArray, index);
+    return outputArray;
+}
+string PrintArray (string [] array)
+{
+    string output = String.Empty;
+    int length = array.Length;
+    for (int i=0 ; i < length ; i++)
+    {
+        output += array[i] + " ";
+    }
+    return output;
+}
+int count = 4;
+string[] inputArray = NewArray(count);
+FillNewArray(inputArray);
+Console.WriteLine ($"[{PrintArray(inputArray)}]");
+string[] outputArray = OutputArray (inputArray);
+Console.Write($" -> [{PrintArray(outputArray)}]");
