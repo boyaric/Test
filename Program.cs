88709aded15e26b1fdf46320268e2814145a859c
Sergei_Kravtsov <boyaric@gmail.com>
2022-07-18 23:40:51 +0300

    array filling

--- Program.cs
+++ Program.cs
@@ -2,4 +2,12 @@
 {
     return new string[count];
 }
-
+void FillNewArray (string[] inputArray)
+{
+    int count = inputArray.Length;
+    Console.WriteLine("Введите строки");
+    for (int i=0; i<count; i++)
+    {
+        inputArray [i] = Console.ReadLine();
+    }
+}
